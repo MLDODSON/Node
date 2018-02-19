0bd51ac9799bfe222773ea91e71d0966c1921b57
MLDODSON <mldodson@fullsail.edu>
2018-02-19 15:35:36 -0600

    Importing/Exporting your own files in node

--- notes.js
+++ notes.js
@@ -2,19 +2,26 @@
 
 
 
-console.log("Starting App!");
+console.log("Starting notes.js!");
 
 const fs = require("fs");
 const os = require("os");
+const notes2 = require("./notes2.js");
+
+//var res = notes2.addNote();
+//console.log(res);
+
+var math = notes2.add(8, 2);
+console.log(math);
 
 
-var user = os.userInfo();
+//var user = os.userInfo();
 
 
 //fs.appendFileSync("greetings.txt", "Hello " + user.username + "!");
 
 //Or it can be done this way for ES 6.
-fs.appendFileSync("greetings.txt", `Hello ${user.username}!`);
+//fs.appendFileSync("greetings.txt", `Hello ${user.username}! You are ${notes2.age}.`);
 
 
 /*

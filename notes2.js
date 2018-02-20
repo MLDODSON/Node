64ccb2be744284296af93d1f467ba62e33fb6069
MLDODSON <mldodson@fullsail.edu>
2018-02-20 14:34:06 -0600

    Adding and Saving Notes

--- notes2.js
+++ notes2.js
@@ -1,10 +1,30 @@
 console.log("Starting notes2.js!");
 
+const fs = require("fs");
+
 //module.exports.age = 50;
 
 //This is an arrow function
 var addNote = (title, body) => {
-	console.log("Adding Note", title, body);	
+	var notes = [];
+	var note = {
+		title,
+		body
+	};
+	
+	try {
+		var notesString = fs.readFileSync("notes-data.json");
+		notes = JSON.parse(notesString);
+	} catch (e) {
+		
+	}
+	
+	var duplicateNotes = notes.filter((note) => note.title === title);
+	
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+	}
 };
 
 var getAll = () => {

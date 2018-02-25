03b5adf9d922e0f337f2c271d564886af44e40b4
MLDODSON <mldodson@fullsail.edu>
2018-02-25 13:02:11 -0600

    Reading Notes and Reusability

--- notes2.js
+++ notes2.js
@@ -38,7 +38,9 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-	console.log("Getting note", title);	
+	var notes = fetchNotes();
+  	var filteredNotes = notes.filter((note) => note.title === title);
+	return filteredNotes[0];
 };
 
 var removeNote = (title) => {
@@ -49,10 +51,27 @@ var removeNote = (title) => {
 	return notes.length !== filteredNotes.length;
 };
 
+var logNote = (note) => {
+	console.log("--");
+	console.log("Title: " + note.title);
+	console.log("Body: " + note.body);
+};
+
 
 module.exports = {
 	addNote,
 	getAll,
 	getNote,
-	removeNote
+	removeNote,
+	logNote
 };
+
+
+
+
+
+
+
+
+
+

d79559cc46a2695902ed85407fc8030507e4a22b
MLDODSON <mldodson@fullsail.edu>
2018-04-03 21:37:26 -0500

    call back

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
 	url: "https://maps.googleapis.com/maps/api/geocode/json?address=2107%20East%20Adelaide%20Saint%20Louis",
 	json: true
 }, (error, response, body) => {
-	console.log(body);	
+	console.log(JSON.stringify(body, undefined, 2));	
 });

ad8541cb7682e69990c0be33ca7902254b03493a
MLDODSON <mldodson@fullsail.edu>
2018-04-06 23:02:21 -0500

    weather app call back errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,7 @@ const request = require("request");
 const yargs = require("yargs");
 
 const argv = yargs
-	.option({
+	.options({
 		a: {
 		  demand: true,
 		  alias: "address",
@@ -15,13 +15,20 @@ const argv = yargs
 	.alias("help", "h")
 	.argv;
 
-var encodedAddress = encodeURIComponent(argv.a);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
 	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	
+	if (error) {
+		console.log("Unable to connect to Google servers.");
+	} else if (body.status === "ZERO_RESULTS") {
+		console.log("Unable to find that address.");	   
+	} else if (body.status === "OK") {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+	}
 });

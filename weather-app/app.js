547648f872b016f1701b3e13694cc6774538c5d4
MLDODSON <mldodson@fullsail.edu>
2018-04-07 23:08:21 -0500

    weather app - wiring up weather search

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,4 @@
-
+/*
 const yargs = require("yargs");
 
 const geocode = require("./geocode/geocode");
@@ -24,4 +24,19 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
 		console.log(JSON.stringify(results, undefined, 2));
 	}
 });
+*/
 
+// f640fd409b5ebdb843aec77544476512
+
+const request = require("request");
+
+request({
+	url: "https://api.darksky.net/forecast/f640fd409b5ebdb843aec77544476512/38.5844866,-90.266699",
+	json: true
+}, (error, response, body) => {
+	if (!error && response.statusCode === 200) {
+		console.log(body.currently.temperature);
+	} else {
+		console.log("Unable to fetch weather.");
+	}
+});

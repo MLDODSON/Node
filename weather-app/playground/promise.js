7b850035bb9eef0d41b25d7234fce8b5d0fde6b2
MLDODSON <mldodson@fullsail.edu>
2018-04-18 19:43:41 -0500

    Advanced Promises/Web App

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,7 +1,29 @@
+var asyncAdd = (a, b) => {
+	return new Promise((resolve, reject) => {
+	  setTimeout(() => {
+		 if (typeof a === "number" && typeof b === "number") {
+			 resolve(a + b);
+		 } else {
+			 reject("Arguments must be numbers.");
+		 }
+	  }, 2000);	
+	});
+};
+
+asyncAdd(5,"7").then((res) => {
+	console.log("Result: ", res);
+	return asyncAdd(res, 35);
+}).then((res) => {
+	console.log("Should be 47.", res);
+}).catch((errorMessage) => {
+	console.log(errorMessage);
+});
+
+/*
 var somePromise = new Promise((resolve, reject) => {
 	setTimeout(() => {
 		resolve("Hey! This actually worked!");
-		reject("Sorry, unable to fulfill promise.");
+		//reject("Sorry, unable to fulfill promise.");
 	}, 3000);
 });
 
@@ -10,3 +32,4 @@ somePromise.then((message) => {
 	}, (errorMessage) => {
 	console.log("Error: ", errorMessage);
 });
+*/
